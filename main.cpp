b69cf77a4ccf8303ebffe43085f9ee90dcb90f5c
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-01 22:14:51 +0000

    User input accepted

--- main.cpp
+++ main.cpp
@@ -4,28 +4,35 @@ using namespace std;
 
 /////////////////////// Function Prototype ////////////////////
 
-
 ////////////////////////// Main Code /////////////////////////
 int main() {
 
   // declare stuct
-  struct Car
-  {
+  struct Car {
     int CarNo;
     int Miles;
     int GalUsed;
   };
 
   // array of 5 structs to accept user input
-
-  Car info[5];
+  Car CarInfo[5];
 
   // for statement to loop through user input
+  for (int i = 0; i < 5; i++) {
+    cout << "Please enter the info for the car. " << endl;
+    cout << "Car Number: ";
+    cin >> CarInfo[i].CarNo;
+    cout << "Miles: ";
+    cin >> CarInfo[i].Miles;
+    cout << "Gallons Used: ";
+    cin >> CarInfo[i].GalUsed;
+    cout << endl;
+  }
 
   //
-  
 }
 
 ///////////////////// User Defined Functions //////////////////
 
-// User defined function to find each car's miles per gal and average miles per gal for entire fleet
+// User defined function to find each car's miles per gal and average miles per
+// gal for entire fleet
