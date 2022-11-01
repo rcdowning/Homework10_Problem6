45c9f5acd60d97f68f9c0d1e214af454f837797f
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-01 22:56:25 +0000

    Each car's MPG

--- .ccls-cache/@home@runner@Problem-6/main.cpp
+++ .ccls-cache/@home@runner@Problem-6/main.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-/////////////////////// Function Prototype ////////////////////
-
 ////////////////////////// Main Code /////////////////////////
 int main() {
+  // declare variables
+  double MPG, AVG;
 
   // declare stuct
   struct Car {
@@ -29,10 +29,41 @@ int main() {
     cout << endl;
   }
 
-  //
+  // loop for car's mpg
+  for (int k = 0; k < 5; k++) {
+    MPG = (CarInfo[k].Miles) / (CarInfo[k].GalUsed);
+
+    cout << "Car number " << CarInfo[k].CarNo << " gets " << MPG
+         << " miles per gallon. " << endl;
+  }
 }
 
-///////////////////// User Defined Functions //////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //////////////////// User Defined Functions //////////////////
 
 // User defined function to find each car's miles per gal and average miles per
-// gal for entire fleet
+gal for entire fleet
+
+void CarReport(Car) {
+
+  // declare variables used inside function
+  double MPG;
+
+  // loop to determin MPG for each
+  for (int i = 0; i < 5; i++){
+    MPG = Car[0].Carno
+  }
+  }
+*/
