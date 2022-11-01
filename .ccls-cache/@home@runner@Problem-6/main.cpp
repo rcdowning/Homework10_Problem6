b4434c7886800b5ed44eef018e039de9e7b42941
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-11-01 21:52:49 +0000

    Laying out code

--- .ccls-cache/@home@runner@Problem-6/main.cpp
+++ .ccls-cache/@home@runner@Problem-6/main.cpp
@@ -2,14 +2,30 @@
 
 using namespace std;
 
+/////////////////////// Function Prototype ////////////////////
+
+
+////////////////////////// Main Code /////////////////////////
 int main() {
 
-  struct
+  // declare stuct
+  struct Car
   {
     int CarNo;
     int Miles;
     int GalUsed;
-  } car;
+  };
 
+  // array of 5 structs to accept user input
+
+  Car info[5];
+
+  // for statement to loop through user input
+
+  //
   
 }
+
+///////////////////// User Defined Functions //////////////////
+
+// User defined function to find each car's miles per gal and average miles per gal for entire fleet
